91260fc7597d5743dc00d9f80077f4e521894116
Gieted <gieted@proton.me>
2022-10-31 22:48:26 +0100

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function visualOffset(r: number, o: number) {
+    const diff = r - o;
+    return diff / (Math.PI * Math.PI);
+}
+
+function App() {
+    const [moonWidth, setMoonWidth] = useState(() => 35);
+    const position = 150;
+    const moonSize = 100;
+    const r = moonSize / 2;
+    const lastPoint = position + moonWidth / 2;
+    const center = position - (lastPoint - position - r) / 2 - visualOffset(moonSize, moonWidth);
+
+    return (
+        <div className="App">
+            <svg height={500} width={500}>
+                <circle fill='#1D1D1D' r={100} cx={position} cy={position}/>
+                <mask id='moon' height={position} width={position}>
+                    <circle fill='white' r={r + moonWidth} cx={center} cy={position}/>
+                    <circle fill='black' r={r + 1} cx={center + moonWidth} cy={position}/>
+                </mask>
+                <circle fill='#FF8E09' r={r} cx={center} cy={position} mask="url(#moon)"/>
+            </svg>
+            <input type='range' min={0} max={101} style={{width: 500}} value={moonWidth}
+                   onInput={(event) => setMoonWidth(+event.currentTarget.value)}/>
+        </div>
+    );
+}
+
+export default App;
